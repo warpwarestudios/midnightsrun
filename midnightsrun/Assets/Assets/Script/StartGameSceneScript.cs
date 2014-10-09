369a2fdc593ad5eb8d5c4b89bffdc2b1b01d0642
Steven Burke <tsunayoshi1234@gmail.com>
2014-10-08 21:14:06 -0500

    Added an temporary Game world
    Added a collision on ground and added a sprite named Midnight

---
+++ midnightsrun/Assets/Assets/Script/StartGameSceneScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGameSceneScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnClick () {
+		Application.LoadLevel ("Main Game");
+	}
+}
