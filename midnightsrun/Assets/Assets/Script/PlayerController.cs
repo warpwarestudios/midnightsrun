c34121efb77fdc22e2b2db6111d63b0f530f98ee
Raigir <Hammontreeryan@gmail.com>
2014-10-10 20:16:16 -0500

    Cat Sprite
    Initial Cat Sprite
    Jumping is not animated yet.

---
+++ midnightsrun/Assets/Assets/Script/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController: MonoBehaviour {
+
+	//animator
+	private Animator animator;
+
+	// standard movement values
+	public float maxSpeed = 10f;
+	public bool facingRight = false;
+
+	// values for jumping
+	public bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
+	public float jumpForce = 700f;
+
+	
+	// Use this for initialization
+	void Start()
+	{
+		Flip ();
+		animator = GetComponent<Animator>();
+	}
+	
+	// FixedUpdate updates at regular intervals
+	void FixedUpdate()
+	{
+		// if grounded = true, on ground
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+		animator.SetBool ("Ground", grounded);
+
+		animator.SetFloat ("vSpeed", rigidbody2D.velocity.y);
+
+		// enables sprite movement
+		float move = Input.GetAxis ("Horizontal");
+		animator.SetFloat ("Speed", Mathf.Abs (move));
+		rigidbody2D.velocity = new Vector2 (move * maxSpeed,0);
+
+		//Flips Sprite
+		if (move > 0 && !facingRight) 
+			Flip ();
+		else if (move < 0 && facingRight) 
+			Flip ();
+		
+	}
+
+	// Update updates at irregular intervals for maximum accuracy
+	void Update()
+	{
+		// standing on ground and space is press, jump
+		if (grounded && Input.GetKeyDown (KeyCode.Space)) 
+		{
+			animator.SetBool ("Ground", false);
+			rigidbody2D.AddForce(new Vector2(0,jumpForce));
+		}
+	}
+	
+	// Flips Sprite
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector2 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+	}
+}
